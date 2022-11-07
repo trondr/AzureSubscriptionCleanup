77df51b7d4a60e49bfe81d0cdd0edcea1ab70311
trondr <trondr@outlook.com>
2022-11-07 02:34:50 +0100

    Cleanup expired resource groups.

--- InvokeAzureSubscriptionCleanup/run.ps1
+++ InvokeAzureSubscriptionCleanup/run.ps1
@@ -9,5 +9,21 @@ if ($Timer.IsPastDue) {
     Write-Host "PowerShell timer is running late!"
 }
 
-# Write an information log with the current time.
-Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
+Write-Host "Cleaning up any expired resource groups."
+$expiredResourceGroups = Get-AzResourceGroup | Where-Object {
+        $($null -ne $($_.Tags.expireOn)) #ExpireOn tag exists
+    } | Where-Object {
+        $expireOn = $([DateTime]$_.Tags.expireOn)
+        $null -ne $expireOn #ExpireOn is a valid date time
+    } | Where-Object {
+        $expireOn = $([DateTime]$_.Tags.expireOn)
+        $now = $(Get-Date)
+        ($expireOn -lt $now) #ExpireOn datetime is in the passed => resource group has expired.
+    }
+$expiredResourceGroups | ForEach-Object {
+    $resourceGroupName = $_.ResourceGroupName
+    Write-Host "Removing expired resource group '$resourceGroupName'..."
+    $isSucceful = Remove-AzResourceGroup -Name $resourceGroupName -Force
+    Write-Host "Resource group '$resourceGroupName' was removed: $isSucceful"
+}
+Write-Host "Subscription cleanup ran at : $((Get-Date).ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss")) (UTC)"
